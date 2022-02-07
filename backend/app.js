21574a221987db453b2a308ef0e3fc67c527fdbd
Joaquin144 <vvpp898.comco@gmail.com>
2022-02-07 22:24:47 +0530

    contorllers/routers/models

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 const tasks = require('tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
 
 
 //      MiddleWares :--
 app.use(express.json())
 
+
+app.use('/api/v1/tasks',tasks)//to direct correct url trhough tasks router
+
 //todo : write a function to start the app
 const start = async () => {
     try{
         //todo : connect MongoDB here only after whole skeleton of app is done
+        await connectDB(process.env.MONGO_URI)
         app.listen(port,console.log(`app is listening at port: ${port}...`))
     }
     catch(error){
