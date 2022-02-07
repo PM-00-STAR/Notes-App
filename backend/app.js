fde3a8792f8d7acf90c2829bb16824261b63a0d2
Joaquin144 <vvpp898.comco@gmail.com>
2022-02-07 23:01:18 +0530

    Server ready for testing

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const tasks = require('tasks')
-const connectDB = require('./db/connect')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 
 
@@ -11,16 +10,20 @@ require('dotenv').config()
 app.use(express.json())
 
 
+//      Routes :----
 app.use('/api/v1/tasks',tasks)//to direct correct url trhough tasks router
+const port = process.env.PORT || 3000;
+
 
 //todo : write a function to start the app
 const start = async () => {
     try{
         //todo : connect MongoDB here only after whole skeleton of app is done
         await connectDB(process.env.MONGO_URI)
-        app.listen(port,console.log(`app is listening at port: ${port}...`))
-    }
-    catch(error){
+        app.listen(port,() => 
+            console.log(`Server is listening on port ${port}...`)
+        )
+    }catch(error){
         console.log(error)
     }
 }
