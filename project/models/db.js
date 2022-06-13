35e01472970f4566593ce27474d6983a471b39f4
pratibhamitra01@gmail.com <pratibhamitra01@gmail.com>
2022-06-13 07:54:33 +0530

    commit

--- project/models/db.js
+++ project/models/db.js
@@ -1,17 +1,18 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-// mongoose.connect(url, { useNewUrlParser: true ,useUnifiedTopology: true, useFindAndModify: false } , (err) => {
+// mongoose.connect(MONGO_URI, { useNewUrlParser: true ,useUnifiedTopology: true, useFindAndModify: false } , (err) => {
 //     if (!err) { console.log('MongoDB Connection Succeeded.') } //callback function
 //     else { console.log('Error in DB connection : ' + err) }
 // });
-const connectDB = (url) => {
-    return mongoose.connect(url,{ useNewUrlParser: true ,useUnifiedTopology: true, useFindAndModify: false } , (err) => {
+const connectDB = () => {
+    return mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true ,useUnifiedTopology: true, useFindAndModify: false } , (err) => {
             if (!err) { console.log('MongoDB Connection Succeeded.') } //callback function
             else { console.log('Error in DB connection : ' + err) }
          });
-}
+ }
 
 
 
-require('./Student.model');
+require('./student.model');
 module.exports = connectDB
--- project/server.js
+++ project/server.js
@@ -1,5 +1,7 @@
 
 require('dotenv').config();
+const mongoose = require('mongoose');
+
 const connectDB = require('./models/db');
 const express = require('express');
 const path = require('path');
